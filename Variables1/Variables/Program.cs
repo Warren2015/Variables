38386a27dddbfbf4eb1c6f5eea06f234eae03b27
bluebeacon8402 <crsorge@icloud.com>
2020-10-22 14:58:52 -0400

    new lesson

---
+++ Variables1/Variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string muppet = "Kermit";
+            int age = 8;
+            char bloodType = 'A';
+            bool isPuppet = false;
+            double howManyExist = 1;
+            decimal money = 5.25m;
+
+            Console.WriteLine($"My muppet's name is {muppet}, " +
+                $"I am {age} years old, my blood type is {bloodType}, you are {isPuppet}, " +
+                $"2 is better than {howManyExist}, do you want {money} in peso?");
+        }
+    }
+}
